0c024b9c88b7f3f5bab0948fd3e922b005e78905
carter-willey <cjwilley23@gmail.com>
2021-07-16 10:20:59 -0500

    added initial react files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <h1>Hello</h1>
+     );
+  }
+}
+ 
+export default App;
