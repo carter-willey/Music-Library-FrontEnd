37bf7b33513150c3dfa31e5a3614465fe0785e26
BA_CalderonMorales <tbcm640@gmail.com>
2021-07-20 09:20:31 -0500

    Fixed small bug with Add Song button in CreateSongForm.

--- src/Components/CreateSongForm/createSongForm.jsx
+++ src/Components/CreateSongForm/createSongForm.jsx
@@ -29,21 +29,21 @@ class CreateSongForm extends Component {
         <div className="col-md-6 bg-light">
           <form className="row g-3 align-items-center p-2" onSubmit={this.handleSubmit} >
             <label className="lead d-flex justify-content-center p-2">Song Title: </label>
-            <input className="bg-light form-control form-control-lg" type="text" name="title" placeholder="Yellow Submarine" 
-            onChange={this.handleChange} value={this.state.title} />
+              <input className="bg-light form-control form-control-lg" type="text" name="title" placeholder="Yellow Submarine" 
+              onChange={this.handleChange} value={this.state.title} />
             <label className="lead d-flex justify-content-center p-2">Artist: </label>
-            <input className="bg-light form-control form-control-lg" type="text" name="artist" placeholder="The Beatles" 
-            onChange={this.handleChange} value={this.state.artist} />
+              <input className="bg-light form-control form-control-lg" type="text" name="artist" placeholder="The Beatles" 
+              onChange={this.handleChange} value={this.state.artist} />
             <label className="lead d-flex justify-content-center p-2">Genre: </label>
-            <input className="bg-light form-control form-control-lg" type="text" name="genre" placeholder="Psychadelic Rock" 
-            onChange={this.handleChange} value={this.state.genre} />
+              <input className="bg-light form-control form-control-lg" type="text" name="genre" placeholder="Psychadelic Rock" 
+              onChange={this.handleChange} value={this.state.genre} />
             <label className="lead d-flex justify-content-center p-2">Album Title: </label>
-            <input className="bg-light form-control form-control-lg" type="text" name="album" placeholder="Yellow Submarine" 
-            onChange={this.handleChange} value={this.state.album} />
+              <input className="bg-light form-control form-control-lg" type="text" name="album" placeholder="Yellow Submarine" 
+              onChange={this.handleChange} value={this.state.album} />
             <label className="lead d-flex justify-content-center p-2">Song Release Date: </label>
-            <input className="bg-light form-control form-control-lg" type="date" name="release_date" 
-            onChange={this.handleChange} value={this.state.release_date} />
-            <button type="button" className="lead btn btn-primary btn-lg" submit="submit">Add Song</button>
+              <input className="bg-light form-control form-control-lg" type="date" name="release_date" 
+              onChange={this.handleChange} value={this.state.release_date} />
+            <button className="lead btn btn-primary btn-lg" submit="submit">Add Song</button>
           </form>
         </div>
       </React.Fragment>
